fb9db7351f54f8b93ee01cdd447d632ac26dff29
Tyler Shimko <TShimko126@gmail.com>
2014-07-15 11:49:05 -0500

    Add structured permutation code from Matt Rockman

--- MappingPipeline.R
+++ MappingPipeline.R
@@ -1,3 +1,5 @@
+options(echo=TRUE)
+
 require(dplyr)
 require(qtl)
 require(stringr)
@@ -32,24 +34,6 @@ map <- function(x, set){
     }
 }
 
-
-perm <- function(x, set){
-    if(set==1){
-        newCross <- subset(N2xCB4856.cross, ind=as.character(N2xCB4856.cross$pheno$rep)=="A")
-    } else if(set == 2){
-        newCross <- subset(N2xCB4856.cross, ind=as.character(N2xCB4856.cross$pheno$rep) %in% c("B", "C", "D"))
-    } else if(set==3){
-        newCross <- subset(N2xCB4856.cross, ind=as.numeric(N2xCB4856.cross$pheno$set)==2)
-    try({if (!all(is.na(newCross$pheno[,x]))) {
-        threshold = summary(scanone(newCross, pheno.col = x, model="np", n.perm = 1000))[1]
-        colName <- colnames(N2xCB4856.cross$pheno)[x]
-        condition <- str_split(colName, "\\.", 2)[[1]][1]
-        trait <- str_split(colName, "\\.", 2)[[1]][2]
-        df <- as.data.frame(cbind(condition, trait, threshold))
-        return(df)
-    }})
-}
-
 mergePheno2 <- function(cross, phenotype, set=NULL){
     if(!is.null(set)){
         phenotype = phenotype %>% filter(set == set)
@@ -66,13 +50,52 @@ renameCols <- function(x){
     return(x)
 }
 
+structperm <- function(cross, chr, pheno.col=1, model=c("normal","binary","2part","np"), perms,
+                       method=c("em","imp","hk","ehk","mr","mr-imp","mr-argmax"),
+                       addcovar=NULL, intcovar=NULL, weights=NULL,
+                       use=c("all.obs", "complete.obs"), upper=FALSE,
+                       ties.random=FALSE, start=NULL, maxit=4000,
+                       tol=1e-4, verbose){
+    
+    print(paste0("Pheno.col: ", pheno.col))
+    
+    lastpheno <- which(colnames(cross$pheno)=="rep")
+    newcol <- lastpheno +1
+    res <- matrix(nrow = perms, ncol = 1)
+    temp1 <- subset(cross, ind = (cross$pheno[,lastpheno] == "A"))
+    temp2 <- subset(cross, ind = (cross$pheno[,lastpheno] %in% c("B", "C", "D")))
+    temp3 <- subset(cross, ind = (is.na(cross$pheno[,lastpheno])))
+    
+    for(i in 1:perms){
+        if(i %% 100 == 0){
+            cat("Structured Permutation", i, "\n")
+        }
+        temp1$pheno[,newcol] <- sample(temp1$pheno[,pheno.col])
+        temp2$pheno[,newcol] <- sample(temp2$pheno[,pheno.col])
+        temp3$pheno[,newcol] <- sample(temp3$pheno[,pheno.col])
+        scan1 <- scanone(temp1, chr = chr, pheno = newcol, model = model, method = method, addcovar = addcovar, intcovar = intcovar, weights= weights, use = use, upper = upper, ties.random = ties.random, start = start, maxit = maxit, tol = tol)
+        scan2 <- scanone(temp2, chr = chr, pheno = newcol, model = model, method = method, addcovar = addcovar, intcovar = intcovar, weights= weights, use = use, upper = upper, ties.random = ties.random, start = start, maxit = maxit, tol = tol)
+        scan3 <- scanone(temp3, chr = chr, pheno = newcol, model = model, method = method, addcovar = addcovar, intcovar = intcovar, weights= weights, use = use, upper = upper, ties.random = ties.random, start = start, maxit = maxit, tol = tol)
+        all <- scan1+scan2+scan3
+        res[i,] <- max(all[,3])
+        
+    }
+    
+    rownames(res) <- 1:perms
+    colName <- colnames(cross$pheno)[pheno.col]
+    condition <- str_split(colName, "\\.", 2)[[1]][1]
+    trait <- str_split(colName, "\\.", 2)[[1]][2]
+    return(data.frame(cbind(rownames=NULL, condition, trait, threshold=quantile(res, probs=.95)[1])))
+    
+}
+
 #---------------Pick your poison-----------------#
 
-pheno <- read.csv("~/Dropbox/HTA/Results/ProcessedData/RIAILs0_complete.csv")
+# pheno <- read.csv("~/Dropbox/HTA/Results/ProcessedData/RIAILs0_complete_simple.csv")
 
 # pheno <- read.csv("~/Downloads/RIAILs0_complete.csv")
 
-# pheno <- read.csv("~/Dropbox/HTA/Results/ProcessedData/RIAILs1_complete.csv")
+pheno <- read.csv("~/Dropbox/HTA/Results/ProcessedData/RIAILs1_complete_simple.csv")
 
 #------------------Process----------------------#
 
@@ -99,54 +122,18 @@ N2xCB4856.cross$pheno <- data.frame(cbind(rep = sapply(mergePheno2(N2xCB4856.cro
 #---------Three sets--------#
 
 system.time(set1Map <- do.call(rbind, lapply(6:ncol(N2xCB4856.cross$pheno), function(x){map(x, 1)})))
-system.time(set1Perm <- do.call(rbind, lapply(6:ncol(N2xCB4856.cross$pheno), function(x){perm(x, 1)})))
 
 system.time(set2Map <- do.call(rbind, lapply(6:ncol(N2xCB4856.cross$pheno), function(x){map(x, 2)})))
-system.time(set2Perm <- do.call(rbind, lapply(6:ncol(N2xCB4856.cross$pheno), function(x){perm(x, 2)})))
 
 system.time(set3Map <- do.call(rbind, lapply(6:ncol(N2xCB4856.cross$pheno), function(x){map(x, 3)})))
-system.time(set3Perm <- do.call(rbind, lapply(6:ncol(N2xCB4856.cross$pheno), function(x){perm(x, 3)})))
+
+
 
 totalMap <- data.frame(do.call(rbind, list(set1Map, set2Map, set3Map)))
 totalMap$lod <- as.numeric(as.character(totalMap$lod))
 masterMap <- totalMap %>% group_by(condition, trait, chr, pos, marker) %>% summarise(lod = sum(lod))
 
-set1Perm <- read.csv("~/LinkageMapping/set1Perm.csv")
-set2Perm <- read.csv("~/LinkageMapping/set2Perm.csv")
-set3Perm <- read.csv("~/LinkageMapping/set3Perm.csv")
-
-allPerm <- data.frame(do.call(rbind, list(set1Perm, set2Perm, set3Perm)))
-allPerm$threshold <- as.numeric(as.character(allPerm$threshold))
-thresholds <- allPerm %>% group_by(condition, trait) %>% summarise(threshold = sum(threshold))
-    
-# #---------Two sets----------#
-# 
-# system.time(set1Map <- do.call(rbind, lapply(6:ncol(N2xCB4856.cross$pheno), function(x){map(x, 4)})))
-# system.time(set1Perm2group <- do.call(rbind, lapply(6:ncol(N2xCB4856.cross$pheno), function(x){perm(x, 4)})))
-# 
-# system.time(set2Map <- do.call(rbind, lapply(6:ncol(N2xCB4856.cross$pheno), function(x){map(x, 5)})))
-# system.time(set2Perm2group <- do.call(rbind, lapply(6:ncol(N2xCB4856.cross$pheno), function(x){perm(x, 5)})))
-# 
-# allPerm2$threshold <- as.numeric(as.character(allPerm2$threshold))
-# thresholds2 <- allPerm2 %>% group_by(condition, trait) %>% summarise(threshold = sum(threshold))
-# 
-# # #------One set--------#
-# 
-# system.time(masterMap <- do.call(rbind, lapply(6:ncol(N2xCB4856.cross$pheno), function(x){map(x, 6)})))
-
-################
-################
-################    2
-################
-################
-################
-
-# data = N2xCB4856.cross$pheno
-# data$group <- ifelse(data$set==2, 3, ifelse(data$rep=="A", 1, ifelse(data$rep %in% c("B", "C", "D"), 2, NA)))
-# 
-# ggplot(data, aes(x = factor(group), y = control.f.ad)) + geom_violin() + geom_boxplot(width=.1)
-# 
-# ggplot(data, aes(x = factor(group), y = control.q10.EXT)) + geom_violin() + geom_boxplot(width=.1)
+thresholds <- do.call(rbind, lapply(6:ncol(N2xCB4856.cross$pheno), function(x){structperm(cross=N2xCB4856.cross, pheno.col=x, model="np", perms=1000)}))
 
 traits = unique(masterMap[,1:2])
 
@@ -165,4 +152,6 @@ for(row in 1:nrow(traits)){
     try(ggsave(plot = plot2, filename = fileName2, width = 11, height = 4.5))
 }
 
+
+write.csv(thresholds, file.path("~/Dropbox/HTA/Results/ProcessedData", "RIAILs0_thresholds.csv"), row.names=FALSE)
 write.csv(masterMap, file.path("~/Dropbox/HTA/Results/ProcessedData", "RIAILs0_MasterMap.csv"), row.names=FALSE)
