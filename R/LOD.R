8860d28c529ff224c06653feb245777491b25dc4
Tyler Shimko <TShimko126@gmail.com>
2015-07-14 07:26:28 -0500

    Change spacing and syntax styling for LOD.R

--- R/LOD.R
+++ R/LOD.R
@@ -9,11 +9,11 @@
 #' graphics card with the gputools package installed.
 #' @return The LOD scores for all markers
 
-get_lod_by_cor = function(npheno, pheno, gdata, doGPU=FALSE) {
+get_lod_by_cor <- function(npheno, pheno, gdata, doGPU=FALSE) {
     if(doGPU) {
-        return((-n.pheno*log(1-gputools::gpuCor(pheno, gdata, use = 'pairwise.complete.obs')$coefficients ^ 2)) / (2 * log(10)))
+        return((-npheno*log(1-gputools::gpuCor(pheno, gdata, use = 'pairwise.complete.obs')$coefficients ^ 2)) / (2 * log(10)))
     } else {
-        return((-n.pheno*log(1 - cor(pheno, gdata, use = 'pairwise.complete.obs') ^ 2)) / (2 * log(10)))  
+        return((-npheno*log(1 - cor(pheno, gdata, use = 'pairwise.complete.obs') ^ 2)) / (2 * log(10)))
     }
 }
 
