879a2d940b8024ca6d5f12a2ba736eed76a22344
Tyler Shimko <TShimko126@gmail.com>
2015-07-23 10:39:37 -0500

    Finish documenting and commenting mapping code

--- R/LOD.R
+++ R/LOD.R
@@ -10,7 +10,15 @@
 #' @return The LOD scores for all markers
 
 get_lod_by_cor <- function(npheno, pheno, gdata, doGPU = FALSE) {
+    
+    # Calculate the LODS score for each marker:phenotype combination
     if(doGPU) {
+        
+        # Throw an error if the gputools package is not installed
+        if (!requireNamespace("gputools", quietly = TRUE)) {
+            stop("The gputools package is required to complete a mapping with 
+                 `doGPU` set to `TRUE`. Please install gputools and try again.")
+        }
         return((-npheno * log(1 - gputools::gpuCor(pheno, gdata, use = 'pairwise.complete.obs')$coefficients ^ 2)) / (2 * log(10)))
     } else {
         return((-npheno * log(1 - cor(pheno, gdata, use = 'pairwise.complete.obs') ^ 2)) / (2 * log(10)))
@@ -34,9 +42,10 @@ map <- function(cross, doGPU = FALSE) {
     npheno <- count_strains_per_trait(scaledpheno)
     geno <- extract_genotype(cross)
     
-    # Do and return the mapping
+    # Do the mapping
     lods <- get_lod_by_cor(npheno, scaledpheno, geno, doGPU)
     
+    # Convert the map result to a scanone object
     lods <- lodmatrix2scanone(lods, cross)
     
     return(lods)
@@ -54,35 +63,63 @@ map <- function(cross, doGPU = FALSE) {
 #' @export
 
 fsearch <- function(cross, iterations = 1000, doGPU = FALSE) {
+    # Set up the iteration count
     iteration <- 1
+    
+    # Complete the first mapping with FDR calculation
     lods <- map(cross)
     fdr <- get_peak_fdr(lods, cross, iterations, doGPU)
     peaks <- get_peaks_above_thresh(lods, fdr)
+    
+    # If there are more than 0 peaks in the map...
     if (nrow(peaks) > 0) {
+        
+        # Create a list to hold all of the mapping iterations
         lodslist <- list()
+        
+        # While the number of peaks in each iteration is greather than 0,
+        # continue with the forward search
         while (nrow(peaks) > 0){
+            
+            # Bind the threshold and iteration information to the map data,
+            # melt the resulting data frame, and add it to the lodslist
             lods <- data.frame(lods)
             lodswiththresh <- cbind(lods, fdr) %>%
                 dplyr::mutate(marker = rownames(.), threshold = fdr, iteration = iteration)
             meltedlods <- tidyr::gather(lodswiththresh, trait, lod, -chr, -pos, -marker, -threshold, -iteration) %>%
                 dplyr::select(marker, chr, pos, trait, lod, threshold, iteration)
             lodslist <- append(lodslist, list(meltedlods))
+            
+            # Get the residual phenotype cvalues and put them back in the cross
+            # object
             resids <- get_pheno_resids(lods, cross, fdr)
             metapheno <- cross$pheno[1:5]
             cross$pheno <- data.frame(metapheno, resids)
+            
+            # Repeat the mapping, FDR calculationa and peak finding
             lods <- map(cross)
             fdr <- get_peak_fdr(lods, cross, iterations, doGPU)
             peaks <- get_peaks_above_thresh(lods, fdr)
+            
+            # Add one to the iteration
             iteration <- iteration + 1
         }
+        
+        # rbind the whole list of maps
         finallods <- dplyr::rbind_all(lodslist)
     } else {
+        
+        # If no peaks were found from the first iteration, just melt the lods
+        # and return them
         lods <- data.frame(lods)
         lodswiththresh <- cbind(lods, fdr) %>%
             dplyr::mutate(marker = rownames(.), threshold = fdr, iteration = 1)
         finallods <- tidyr::gather(lodswiththresh, trait, lod, -chr, -pos, -marker, -threshold, -iteration) %>%
             dplyr::select(marker, chr, pos, trait, lod, threshold, iteration)
     }
+    
+    # Switch genetic position to physical position (WS244) for all of the
+    # markers in the map
     cat("\nConverting marker position to physical position. This step takes a while...\n")
     finallods$pos <- vapply(finallods$marker, function(marker) {
             return(as.numeric(unlist(
