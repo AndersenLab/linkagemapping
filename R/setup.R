7e0b3bbb63994ede923c96f2d8be45c9bd59f1ad
Tyler Shimko <TShimko126@gmail.com>
2015-08-04 14:06:03 -0500

    Change genotypes to factors

--- R/setup.R
+++ R/setup.R
@@ -71,25 +71,8 @@ mergepheno <- function(cross, phenotype, parents, set=NULL){
 extract_genotype=function(cross){
     
     # Pull out the genotypes into snp x strain matrix
-    genomat <- do.call(cbind, sapply(cross$pheno, function(x) {
-            x$data
-    }))
-    
-    genomat <- apply(genomat, c(1,2), function(y) {
-        
-        # Create a list to use to convert between the numbers
-        genos <- list("1" = -1, "2" = 1, "3" = 0)
-        
-        # If the list entry for the number is not in the dictionary, replace
-        # with NA
-        if (is.null(genos[[as.character(y)]])) {
-            return(NA)
-        } else {
-            
-            # Otherwise pull the correct genotype notation from the list
-            return(genos[[as.character(y)]])
-        }
-    })
+    genomat <- qtl::pull.geno(cross)
+    genomat <- as.factor(genomat)
     return(genomat)
 }
 
