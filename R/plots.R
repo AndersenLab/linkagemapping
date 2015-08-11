5112e5b1fcd9bacef766e8ca27ec9f0af78a4647
Shannon Brady <shannonbrady2014@u.northwestern.edu>
2015-08-11 11:27:20 -0500

    Color triangles by iteration in lodplot, find all peaks in maxlodplot, added ggplot2:: to all mapping functions

--- R/plots.R
+++ R/plots.R
@@ -7,50 +7,51 @@
 #' @export
 
 lodplot <- function(map){
-    cis <- map %>% 
+    
+    maxmap <- mean.TOF %>%
+            dplyr::group_by(marker) %>%
+            dplyr::filter(lod == max(lod))
+    
+    cis <- mean.TOF %>% 
         dplyr::group_by(iteration) %>%
         dplyr::filter(!is.na(var_exp)) %>%
         do(head(., n=1))
     
-    maxmap <- map %>%
-            dplyr::group_by(marker) %>%
-            dplyr::filter(lod == max(lod))
-    
     maxmap <- cidefiner(cis, maxmap)
     
-    plot <- ggplot(map)
+    plot <- ggplot2::ggplot(map)
+    plot <- plot + 
+        ggplot2::geom_line(ggplot2::aes(x = pos/1e6, y = lod, colour = as.factor(iteration)),
+                           size = 1, alpha = 0.85) +
+        ggplot2::facet_grid(.~chr, scales ="free") +
+        ggplot2::labs(x = "Position (Mb)", y = "LOD") +
+        ggplot2::scale_colour_discrete(name="Mapping\nIteration") +
+        ggplot2::ggtitle(map$trait[1]) +
+        ggplot2::theme_bw() +
+        ggplot2::theme(axis.text.x = ggplot2::element_text(size=16, face="bold", color="black"),
+                       axis.text.y = ggplot2::element_text(size=16, face="bold", color="black"),
+                       axis.title.x = ggplot2::element_text(size=20, face="bold", color="black", vjust=-.3),
+                       axis.title.y = ggplot2::element_text(size=20, face="bold", color="black"),
+                       strip.text.x = ggplot2::element_text(size=20, face="bold", color="black"),
+                       strip.text.y = ggplot2::element_text(size=20, face="bold", color="black"),
+                       plot.title = ggplot2::element_text(size=24, face="bold", vjust = 1),
+                       panel.background = ggplot2::element_rect( color="black",size=1.2))
     
     if(nrow(cis) != 0) {
         plot <- plot + 
             ggplot2::geom_ribbon(data = maxmap,
-                        aes(x = pos/1e6, ymin = 0, ymax = ci_lod),
-                        fill = "blue", alpha = 0.5) +
-            ggplot2::geom_point(data = cis, ggplot2::aes(x=pos/1e6, y=lod + 0.75),
-                       fill ="red", shape=25, size=3.2, show_guide = FALSE) +
-            geom_text(data = cis,
-                      aes(x=pos/1e6,
-                          y=lod + 1.5,
+                        ggplot2::aes(x = pos/1e6, ymin = 0, ymax = ci_lod, fill="grey"),
+                        alpha = 0.5, show_guide=FALSE) +
+            ggplot2::geom_point(data = cis, ggplot2::aes(x=pos/1e6, y=lod + 1.00, fill=as.factor(iteration)),
+                       shape=25, size=3.2, show_guide = FALSE) +
+            ggplot2::geom_text(data = cis,
+                      ggplot2::aes(x=pos/1e6,
+                          y=lod + 2.5,
                           label = paste0(100*round(var_exp, digits = 4),"%")),
                       colour = "black", size=5)
     }
     
-    plot <- plot + 
-        geom_line(aes(x = pos/1e6, y = lod, colour = as.factor(iteration)),
-                  size = 1, alpha = 0.85) +
-        facet_grid(.~chr, scales ="free") +
-        labs(x = "Position (Mb)", y = "LOD") +
-        scale_colour_discrete(name="Mapping\nIteration") +
-        ggtitle(map$trait[1]) +
-        
-        theme_bw() +
-        theme(axis.text.x = element_text(size=16, face="bold", color="black"),
-              axis.text.y = element_text(size=16, face="bold", color="black"),
-              axis.title.x = element_text(size=20, face="bold", color="black", vjust=-.3),
-              axis.title.y = element_text(size=20, face="bold", color="black"),
-              strip.text.x = element_text(size=20, face="bold", color="black"),
-              strip.text.y = element_text(size=20, face="bold", color="black"),
-              plot.title = element_text(size=24, face="bold", vjust = 1),
-              panel.background = element_rect( color="black",size=1.2))
+
     return(plot)
 }
 
@@ -62,53 +63,58 @@ lodplot <- function(map){
 #' @export
 
 maxlodplot <- function(map){
-    map <- map %>%
+    map1 <- map %>%
         dplyr::group_by(marker) %>%
         dplyr::filter(lod == max(lod))
     
-    cis <- map %>% 
+    cis <- map %>%
+        dplyr::group_by(marker) %>%
+        dplyr::mutate(maxlod=max(lod))%>%
         dplyr::group_by(iteration) %>%
         dplyr::filter(!is.na(var_exp)) %>%
         do(head(., n=1))
     
+    
     if(nrow(cis) == 0) {
-        plot <- ggplot(map, aes(x = genotype, y = pheno)) + geom_blank()
+        plot <- ggplot2::ggplot(map1, ggplot2::aes(x = genotype, y = pheno)) + ggplot2::geom_blank()
         return(plot)
     }
     
-    map <- cidefiner(cis, map)
+    map1 <- cidefiner(cis, map1)
     
-    plot <- ggplot(map) +
-        aes(x = pos/1e6, y = lod)
+    
+    
+    plot <- ggplot2::ggplot(map1) +
+        ggplot2::aes(x = pos/1e6, y = lod)
     
     if(nrow(cis) != 0) {
         plot <- plot + 
-            geom_ribbon(aes(x = pos/1e6, ymin = 0, ymax = ci_lod),
+            ggplot2::geom_ribbon(ggplot2::aes(x = pos/1e6, ymin = 0, ymax = ci_lod),
                         fill = "blue", alpha = 0.5) +
-            geom_point(data = cis, aes(x=pos/1e6, y=lod + 0.75),
+            ggplot2::geom_point(data = cis, ggplot2::aes(x=pos/1e6, y=maxlod + 1.00),
                        fill ="red", shape=25, size=3.2, show_guide = FALSE) +
-            geom_text(data = cis,
-                      aes(x=pos/1e6,
-                          y=lod + 1.5,
+            ggplot2::geom_text(data = cis,
+                               ggplot2::aes(x=pos/1e6,
+                          y=lod + 2.5,
                           label = paste0(100*round(var_exp, digits = 4),"%")),
                       colour = "black", size=5)
     }
     
-    plot <- plot + geom_line(size = 1, alpha = 0.85) +
-        facet_grid(.~chr, scales ="free") +
-        labs(x = "Position (Mb)", y = "LOD") +
-        scale_colour_discrete(name="Mapping\nIteration") +
-        ggtitle(map$trait[1]) +
+    plot <- plot + ggplot2::geom_line(size = 1, alpha = 0.85) +
+        ggplot2::facet_grid(.~chr, scales ="free") +
+        ggplot2::labs(x = "Position (Mb)", y = "LOD") +
+        ggplot2::scale_colour_discrete(name="Mapping\nIteration") +
+        ggplot2::ggtitle(map1$trait[1]) +
         
-        theme_bw() +
-        theme(axis.text.x = element_text(size=16, face="bold", color="black"),
-              axis.text.y = element_text(size=16, face="bold", color="black"),
-              axis.title.x = element_text(size=20, face="bold", color="black", vjust=-.3),
-              axis.title.y = element_text(size=20, face="bold", color="black"),
-              strip.text.x = element_text(size=20, face="bold", color="black"),
-              strip.text.y = element_text(size=20, face="bold", color="black"),
-              plot.title = element_text(size=24, face="bold", vjust = 1),
-              panel.background = element_rect( color="black",size=1.2))
+        ggplot2::theme_bw() +
+        ggplot2::theme(axis.text.x = ggplot2::element_text(size=16, face="bold", color="black"),
+              axis.text.y = ggplot2::element_text(size=16, face="bold", color="black"),
+              axis.title.x = ggplot2::element_text(size=20, face="bold", color="black", vjust=-.3),
+              axis.title.y = ggplot2::element_text(size=20, face="bold", color="black"),
+              strip.text.x = ggplot2::element_text(size=20, face="bold", color="black"),
+              strip.text.y = ggplot2::element_text(size=20, face="bold", color="black"),
+              plot.title = ggplot2::element_text(size=24, face="bold", vjust = 1),
+              panel.background = ggplot2::element_rect( color="black",size=1.2))
     return(plot)
 }
 
@@ -159,23 +165,23 @@ pxgplot <- function(cross, map) {
     split <- split %>%
         dplyr::mutate(genotype = ifelse(genotype == -1, "N2", "CB4856"))
     
-    ggplot(split) +
-        geom_boxplot(aes(x = genotype, y = pheno, fill = genotype), outlier.shape = NA) +
-        scale_fill_manual(values = c("N2" = "orange", "CB4856" = "blue")) +
-        geom_jitter(aes(x = genotype, y = pheno), alpha = .8) +
-        facet_wrap(~ marker, ncol = 5) +
-        theme_bw() +
-        theme(axis.text.x = element_text(size=16, face="bold", color="black"),
-              axis.text.y = element_text(size=16, face="bold", color="black"),
-              axis.title.x = element_text(size=20, face="bold", color="black", vjust=-.3),
-              axis.title.y = element_text(size=20, face="bold", color="black"),
-              strip.text.x = element_text(size=20, face="bold", color="black"),
-              strip.text.y = element_text(size=20, face="bold", color="black"),
-              plot.title = element_text(size=24, face="bold", vjust = 1),
+    ggplot2::ggplot(split) +
+        ggplot2::geom_boxplot(ggplot2::aes(x = genotype, y = pheno, fill = genotype), outlier.shape = NA) +
+        ggplot2::scale_fill_manual(values = c("N2" = "orange", "CB4856" = "blue")) +
+        ggplot2::geom_jitter(ggplot2::aes(x = genotype, y = pheno), alpha = .8) +
+        ggplot2::facet_wrap(~ marker, ncol = 5) +
+        ggplot2::theme_bw() +
+        ggplot2::theme(axis.text.x = ggplot2::element_text(size=16, face="bold", color="black"),
+              axis.text.y = ggplot2::element_text(size=16, face="bold", color="black"),
+              axis.title.x = ggplot2::element_text(size=20, face="bold", color="black", vjust=-.3),
+              axis.title.y = ggplot2::element_text(size=20, face="bold", color="black"),
+              strip.text.x = ggplot2::element_text(size=20, face="bold", color="black"),
+              strip.text.y = ggplot2::element_text(size=20, face="bold", color="black"),
+              plot.title = ggplot2::element_text(size=24, face="bold", vjust = 1),
               legend.position="none",
-              panel.background = element_rect(color="black",size=1.2)) +
-        ggtitle(peaks$trait[1]) +
-        labs(x = "Genotype", y = "Phenotype")
+              panel.background = ggplot2::element_rect(color="black",size=1.2)) +
+        ggplot2::ggtitle(peaks$trait[1]) +
+        ggplot2::labs(x = "Genotype", y = "Phenotype")
 }
 
 #' Plot effect size by marker
@@ -193,20 +199,20 @@ effectplot <- function(cross, map) {
     
     annotated_map <- annotate_lods(map, cross, annotate_all = TRUE)
     
-    ggplot(annotated_map) +
-        geom_line(aes(x = pos/1e6, y = eff_size), size = 1, alpha = 0.85) +
-        facet_grid(.~chr, scales ="free") +
-        labs(x = "Position (Mb)", y = "Effect Size") +
-        ggtitle(annotated_map$trait[1])+
-        theme_bw() +
-        theme(axis.text.x = element_text(size=16, face="bold", color="black"),
-              axis.text.y = element_text(size=16, face="bold", color="black"),
-              axis.title.x = element_text(size=20, face="bold", color="black", vjust=-.3),
-              axis.title.y = element_text(size=20, face="bold", color="black"),
-              strip.text.x = element_text(size=20, face="bold", color="black"),
-              strip.text.y = element_text(size=20, face="bold", color="black"),
-              plot.title = element_text(size=24, face="bold", vjust = 1),
-              panel.background = element_rect( color="black",size=1.2))
+    ggplot2::ggplot(annotated_map) +
+        ggplot2::geom_line(ggplot2::aes(x = pos/1e6, y = eff_size), size = 1, alpha = 0.85) +
+        ggplot2::facet_grid(.~chr, scales ="free") +
+        ggplot2::labs(x = "Position (Mb)", y = "Effect Size") +
+        ggplot2::ggtitle(annotated_map$trait[1])+
+        ggplot2::theme_bw() +
+        ggplot2::theme(axis.text.x = ggplot2::element_text(size=16, face="bold", color="black"),
+              axis.text.y = ggplot2::element_text(size=16, face="bold", color="black"),
+              axis.title.x = ggplot2::element_text(size=20, face="bold", color="black", vjust=-.3),
+              axis.title.y = ggplot2::element_text(size=20, face="bold", color="black"),
+              strip.text.x = ggplot2::element_text(size=20, face="bold", color="black"),
+              strip.text.y = ggplot2::element_text(size=20, face="bold", color="black"),
+              plot.title = ggplot2::element_text(size=24, face="bold", vjust = 1),
+              panel.background = ggplot2::element_rect( color="black",size=1.2))
 }
 
 
